3974348c7850bd81b78be13c13f238ae37a81722
Pnonato <like.nonato.marrara@gmail.com>
2026-02-22 15:14:06 -0300

    feat: criando execução do programa

---
+++ src/main/java/Main.java
@@ -0,0 +1,155 @@
+/* PARTICIPANTES:
+* Pedro Marrara Barros Nonato
+* Caua Valim De Sousa Olivio
+*
+* */
+
+
+import Persistence.dao.ConnectionFactory;
+import Persistence.dao.H2DataBaseStudentDAO;
+import Persistence.dao.StudentDAO;
+import jakarta.persistence.EntityManager;
+import model.Student;
+import services.RegisterStudentsService;
+import services.RemoveStudentsService;
+import services.UpdateStudentsService;
+import util.Ui;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final Scanner scanner = new Scanner(System.in);
+        final EntityManager entityManager = ConnectionFactory.getEntityManager();
+        final StudentDAO dao = new H2DataBaseStudentDAO(entityManager);
+        final RegisterStudentsService registerStudentsService = new RegisterStudentsService(dao);
+        final UpdateStudentsService updateStudentsService = new UpdateStudentsService(dao);
+        final RemoveStudentsService removeStudentsService = new RemoveStudentsService(dao);
+
+        try {
+            int option;
+
+            do {
+                Ui.mainMenu();
+                System.out.print("Digite a opção desejada: ");
+                option = scanner.nextInt();
+
+                // Limpar buffer
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1:
+                        System.out.println("CADASTRO DE ALUNO:");
+                        System.out.print("Digite o nome: ");
+                        final String name = scanner.nextLine();
+                        System.out.print("Digite o RA (SC00000X): ");
+                        final String ra = scanner.nextLine();
+                        System.out.print("Digite o email (exemplo@email.com): ");
+                        final String email = scanner.nextLine();
+
+                        System.out.print("Digite a nota 1: ");
+                        final BigDecimal grade1 = scanner.nextBigDecimal();
+                        System.out.print("Digite a nota 2: ");
+                        final BigDecimal grade2 = scanner.nextBigDecimal();
+                        System.out.print("Digite a nota 3: ");
+                        final BigDecimal grade3 = scanner.nextBigDecimal();
+
+                        final Student newStudent = new Student(name, ra, email, grade1, grade2, grade3);
+                        registerStudentsService.register(newStudent);
+
+                        break;
+                    case 2:
+                        System.out.println("EXLUIR ALUNO:");
+                        System.out.print("Digite o nome: ");
+                        final String targetName = scanner.nextLine();
+
+                        final Optional<Student> target = dao.findByName(targetName);
+
+                        if (target.isPresent()) {
+                            removeStudentsService.remove(target.get());
+
+                            System.out.println("Aluno removido com sucesso!");
+                        } else {
+                            System.out.println("Aluno não encontrado.");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("ALTERAR ALUNO:");
+                        System.out.print("Digite o nome: ");
+                        final String nameUpdate = scanner.nextLine();
+
+                        final Optional<Student> updateStudent = dao.findByName(nameUpdate);
+
+                        if (updateStudent.isPresent()) {
+                            System.out.println("Dados do aluno:");
+                            Ui.printStudentInfos(updateStudent.get());
+
+                            System.out.println("\n\nNOVOS DADOS:\n\n");
+
+                            System.out.print("Digite o nome: ");
+                            final String newName = scanner.nextLine();
+                            System.out.print("Digite o RA: ");
+                            final String newRa = scanner.nextLine();
+                            System.out.print("Digite o email: ");
+                            final String newEmail = scanner.nextLine();
+
+
+                            System.out.print("Digite a nota 1: ");
+                            final BigDecimal newGrade1 = BigDecimal.valueOf(scanner.nextDouble());
+                            System.out.print("Digite a nota 2: ");
+                            final BigDecimal newGrade2 = BigDecimal.valueOf(scanner.nextDouble());
+                            System.out.print("Digite a nota 3: ");
+                            final BigDecimal newGrade3 = BigDecimal.valueOf(scanner.nextDouble());
+
+                            updateStudentsService.update(updateStudent.get(), newName, newRa, newEmail, newGrade1, newGrade2, newGrade3);
+                        } else {
+                            System.out.println("Aluno não encontrado.");
+                        }
+                        break;
+                    case 4:
+                        System.out.print("Digite o nome: ");
+                        final String nameFound = scanner.nextLine();
+
+                        final Optional<Student> foundStudent = dao.findByName(nameFound);
+
+                        if (foundStudent.isPresent()) {
+                            Ui.printStudentInfos(foundStudent.get());
+                        } else {
+                            System.out.println("Aluno não encontrado.");
+                        }
+
+                        break;
+                    case 5:
+                        final List<Student> students = dao.findAll();
+
+                        if (students.isEmpty()) {
+                            System.out.println("Nenhum aluno cadastrado na base!!!");
+                        } else {
+                            System.out.println("\nExibindo todos os alunos:\n");
+                            for (Student s : students) {
+                                Ui.printStudentInfos(s);
+                            }
+                        }
+
+                        break;
+                    case 6:
+                        System.out.println("Vai curintia!!");
+                        System.out.println("Obrigado Diego Souza #Libertadores2012");
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
+            } while (6 != option);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        scanner.close();
+        entityManager.close();
+    }
+}

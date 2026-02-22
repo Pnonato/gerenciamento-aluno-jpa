d77e5784dbeb4493b9ae9f9051fe1052fe518830
Pnonato <like.nonato.marrara@gmail.com>
2026-02-22 15:13:33 -0300

    feat: criando util

---
+++ src/main/java/util/Ui.java
@@ -0,0 +1,29 @@
+package util;
+
+import model.Student;
+
+public class Ui {
+
+    private Ui() {}
+
+    public static void mainMenu() {
+        System.out.println("\n** CADASTRO DE ALUNOS **\n");
+        System.out.println("1. Cadastrar aluno");
+        System.out.println("2. Excluir aluno");
+        System.out.println("3. Editar aluno");
+        System.out.println("4. Buscar aluno pelo nome");
+        System.out.println("5. Listar alunos (com status de aprovação)");
+        System.out.println("6. Encerrar");
+    }
+
+    public static void printStudentInfos(Student student) {
+        System.out.println();
+        System.out.println("Nome: " + student.getName());
+        System.out.println("RA: " + student.getRa());
+        System.out.println("Email: " + student.getEmail());
+        System.out.printf("Notas: %.2f - %.2f - %.2f\n", student.getGrade1().doubleValue(), student.getGrade2().doubleValue(), student.getGrade3().doubleValue());
+        System.out.printf("Média: %.2f\n", student.getAverage());
+        System.out.println("Situação: " + student.getSituation());
+        System.out.println();
+    }
+}
---
+++ src/main/java/util/Validation.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.math.BigDecimal;
+import java.util.regex.Pattern;
+
+public class Validation {
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_REGEX);
+    private static final String RA_REGEX = "^SC[0-9X]+$";
+    private static final Pattern RA_PATTERN = Pattern.compile(RA_REGEX);
+
+    public static boolean isValidEmail(String email) {
+        return EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean isValidRA(String ra) {
+        return RA_PATTERN.matcher(ra).matches();
+    }
+
+    public static boolean isValidGrade(BigDecimal grade) {
+        final double doubleGrade = grade.doubleValue();
+
+        return doubleGrade >= 0 && doubleGrade <= 10;
+    }
+}
